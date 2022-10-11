7e8a9226f18b4fefb5c889a332ace75c6e456420
y1g4 <yfrank259@gmail.com>
2022-10-11 08:38:21 +0000

    commit

--- github_contributions.py
+++ github_contributions.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from random import randint
 
 github_username = os.getenv("GITHUB_USERNAME")
@@ -10,12 +11,14 @@ if not github_username or not github_token:
 
 repository_url = "https://" + github_username + ":" + github_token + "@github.com/frankyiga/con.git"
 
+# Calculate the initial date
+start_date = datetime.datetime.now() - datetime.timedelta(days=364)
+
 for i in range(1, 365):
     for j in range(0, randint(1, 10)):
-        d = str(i) + ' days ago'
+        d = (start_date + datetime.timedelta(days=i)).strftime('%Y-%m-%dT%H:%M:%SZ')
         with open('file.txt', 'a') as file:
             file.write(d)
         os.system('git add .')
         os.system(f'GIT_AUTHOR_DATE="{d}" GIT_COMMITTER_DATE="{d}" git commit -m "commit"')
         os.system(f'git push {repository_url} main')
-
